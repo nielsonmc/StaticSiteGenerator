f1e12336eadd868dc3a731422690df4b239544ad
nielsonmc <nielsonmc@gmail.com>
2025-06-01 13:39:33 -0600

    add child class ParentNode to HTMLNode, add tests for ParentNode.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,9 +21,9 @@ class HTMLNode:
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
-    
+
     def to_html(self):
         if self.value is None:
             raise ValueError("invalid HTML: no value")
@@ -33,3 +33,21 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: missing tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: children missing")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
