a9f0fa52c2cb64c5ab9dc60f7427b71213aa31ba
nielsonmc <nielsonmc@gmail.com>
2025-06-04 16:29:21 -0600

    Added inline_markdown.py, test_inline_markdown.py

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -5,18 +5,18 @@ from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
         self.assertEqual(node, node2)
-    
+
     def test_eq_false(self):
         node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
     def test_eq_false2(self):
-        node = TextNode("This is a text node", TextType.CODE)
-        node2 = TextNode("This. is. a test", TextType.CODE)
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
         self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
@@ -33,10 +33,10 @@ class TestTextNode(unittest.TestCase):
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
     def test_text(self):
-        node = TextNode("this is a text node", TextType.TEXT)
+        node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "this is a text node")
+        self.assertEqual(html_node.value, "This is a text node")
 
     def test_image(self):
         node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
@@ -44,7 +44,7 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
         self.assertEqual(
-            html_node.props, 
+            html_node.props,
             {"src": "https://www.boot.dev", "alt": "This is an image"},
         )
 
